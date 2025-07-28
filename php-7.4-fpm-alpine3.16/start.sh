d23ba384570ccc1709b33a750839fc58861b5fc8
catphp <>
2025-07-28 16:03:18 +0800

    支持amd64和arm64

--- php-7.4-fpm-alpine3.16/start.sh
+++ php-7.4-fpm-alpine3.16/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # 启动 cat-agent 在后台运行
-#nohup /cat/cat-agent -config=/cat/configs/config.json >/dev/null 2>&1 &
+nohup /cat/cat-agent -config=/cat/configs/config.json >/dev/null 2>&1 &
 
 # 启动 php-fpm 作为主进程
 exec php-fpm
